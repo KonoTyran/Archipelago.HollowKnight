4dabbfc1e5bf084215bb38d4677c1a28b0201af1
Hussein Farran <hmfarran@gmail.com>
2022-04-08 16:20:11 -0400

    Clean up IC based code a bit by reducing unused/unnecessary classes.

---
+++ Archipelago.HollowKnight/IC/VanillaPlacementLocation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ItemChanger.Locations;
+
+namespace Archipelago.HollowKnight.IC
+{
+    internal class VanillaPlacementLocation : AutoLocation
+    {
+        public VanillaPlacementLocation(string name)
+        {
+            this.name = name;
+        }
+
+        protected override void OnLoad()
+        {
+            
+        }
+
+        protected override void OnUnload()
+        {
+            
+        }
+    }
+}
